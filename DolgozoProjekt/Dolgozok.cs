4009e464cc4a997c2414758c00a3dc6418d33fda
anti <kovacsevicsalex@gmail.com>
2026-02-26 14:05:06 +0100

    Add project files.

---
+++ DolgozoProjekt/Dolgozok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozoProjekt
+{
+    public class Dolgozo
+    {
+        public string Vezeteknev;
+        public string Keresztnev;
+        public string Neme;
+        public int Kor;
+        public int Fizetes;
+
+        public Dolgozo(string vezeteknev, string keresztnev, string neme, int kor, int fizetes)
+        {
+            Vezeteknev = vezeteknev;
+            Keresztnev = keresztnev;
+            Neme = neme;
+            Kor = kor;
+            Fizetes = fizetes;
+        }
+    }
+}
---
+++ DolgozoProjekt/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DolgozoProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("adatok.txt");
+            List<Dolgozo> dolgozok = new List<Dolgozo>();
+
+            while (!sr.EndOfStream)
+            {
+                string[] adatok = sr.ReadLine().Split(' ');
+                Dolgozo dolgozo = new Dolgozo(
+
+                   adatok[0],
+                   adatok[1],
+                   adatok[2],
+                   int.Parse(adatok[3]),
+                   int.Parse(adatok[4])
+                );
+                dolgozok.Add(dolgozo);
+
+            }
+
+            Console.WriteLine("3. Feladat: " + dolgozok.Count + " darab dolgozó adata található");
+
+            var atlagFizetes = dolgozok.Where(y => y.Kor < 25).ToList().Sum(x => x.Fizetes);
+            Console.WriteLine("4. Feladat: " + atlagFizetes + " forint");
+
+            var legnagyobbfizetesuDolgozo = dolgozok.OrderByDescending(x => x.Fizetes).First();
+            Console.WriteLine("5. Feladat: \n A legnagyobb fizetésű dolgozó adatai: \n" + " Dolgozó neve: \t" + legnagyobbfizetesuDolgozo.Vezeteknev + " " + legnagyobbfizetesuDolgozo.Keresztnev + "\n" + " Neme: \t" + legnagyobbfizetesuDolgozo.Neme + "\n Életkora: \t" + legnagyobbfizetesuDolgozo.Kor + "\n Fizetése: \t" + legnagyobbfizetesuDolgozo.Fizetes + " Ft");
+
+            Console.WriteLine("6. Feladat: Kérem adjon meg egy összeget:");
+            int osszeg = int.Parse(Console.ReadLine());
+
+            bool vanE = dolgozok.Any(x => x.Fizetes > osszeg);
+            if (vanE)
+            {
+                Console.WriteLine("Van ilyen dolgozó, akinek a fizetése " + osszeg + " Ft felett van");
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen dolgozó");
+
+            }
+        }
+    }
+}
